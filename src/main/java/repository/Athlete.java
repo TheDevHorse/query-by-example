169cf74eb01c8a74ec16cfe80390532c1c5c81c3
hugop <hugopessego@gmail.com>
2025-01-26 14:01:21 +0000

    Add Address class and enhance Athlete entity
    Added a new Address class and updated Athlete to include additional fields (athleteId, age, name, address). Implemented getters, setters, and toString methods in both classes for improved data handling and representation. These updates enhance entity modeling for future expansions.

--- src/main/java/repository/Athlete.java
+++ src/main/java/repository/Athlete.java
@@ -7,5 +7,57 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Athlete {
 
     @Id
-    private Long id;
+    private String athleteId;
+    private int age;
+    private String name;
+    private Address address;
+
+    public Athlete(String athleteId, int age, String name, Address address) {
+        this.athleteId = athleteId;
+        this.age = age;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getAthleteId() {
+        return athleteId;
+    }
+
+    public void setAthleteId(String athleteId) {
+        this.athleteId = athleteId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Athlete{" +
+                "athleteId='" + athleteId + '\'' +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
 }
