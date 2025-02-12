a98ebeabe2a9b1c0f0750dfc23782e6e393cde73
hugop <hugopessego@gmail.com>
2025-02-12 22:11:39 +0000

    Update tests to use AthleteQBERepository
    Replaced AthleteGraphqlQBERepository with AthleteQBERepository in integration tests. Added a new test to validate finding an athlete by example ID. This enhances test clarity and aligns tests with the updated repository.

--- src/test/java/com/thedevhorse/querybyexample/AthleteQBERepositoryTests.java
+++ src/test/java/com/thedevhorse/querybyexample/AthleteQBERepositoryTests.java
@@ -2,18 +2,24 @@ package com.thedevhorse.querybyexample;
 
 import com.thedevhorse.querybyexample.repository.Address;
 import com.thedevhorse.querybyexample.repository.Athlete;
-import com.thedevhorse.querybyexample.repository.AthleteGraphqlQBERepository;
+import com.thedevhorse.querybyexample.repository.AthleteQBERepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AthleteQBERepositoryTests extends AbstractBaseIntegrationTest {
 
     @Autowired
-    private AthleteGraphqlQBERepository athleteGraphqlQBERepository;
+    private AthleteQBERepository athleteQBERepository;
 
     @BeforeEach
     void setUp() {
-        athleteGraphqlQBERepository.deleteAll();
+        athleteQBERepository.deleteAll();
 
         Address address = new Address(
                 "Nova Iorque",
@@ -27,6 +33,25 @@ class AthleteQBERepositoryTests extends AbstractBaseIntegrationTest {
                 "Jake Thompson",
                 address
         );
-        athleteGraphqlQBERepository.save(athlete);
+        athleteQBERepository.save(athlete);
+    }
+
+    @Test
+    void givenValidAthleteId_whenFindById_thenReturnsAthlete() {
+        // Given
+        Example<Athlete> exampleAthlete = Example.of(
+                new Athlete(
+                        "INT-999",
+                        null,
+                        null,
+                        null
+                )
+        );
+
+        // When
+        Optional<Athlete> actual = athleteQBERepository.findOne(exampleAthlete);
+
+        // Then
+        assertTrue(actual.isPresent());
     }
 }
