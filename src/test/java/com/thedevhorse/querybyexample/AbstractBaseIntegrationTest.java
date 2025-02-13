18051b72a96105d8f3f10dccd4c88f536ab026e4
hugop <hugopessego@gmail.com>
2025-02-13 22:58:31 +0000

    Update test setup by removing unused Spring annotations
    Removed redundant `@SpringBootTest` and `@AutoConfigureHttpGraphQlTester` annotations from the base integration test. This simplifies the test configuration and ensures only necessary annotations are used. Cleaned up imports accordingly for better maintainability.

--- src/test/java/com/thedevhorse/querybyexample/AbstractBaseIntegrationTest.java
+++ src/test/java/com/thedevhorse/querybyexample/AbstractBaseIntegrationTest.java
@@ -1,7 +1,5 @@
 package com.thedevhorse.querybyexample;
 
-import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureHttpGraphQlTester;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
